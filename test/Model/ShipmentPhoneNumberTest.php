6bcf499c736d7c7772f1634023162ad25fe8d665
Vasil <vasildakov@gmail.com>
2022-07-07 11:46:03 +0300

    Updated composer.json and project structure

---
+++ test/Model/ShipmentPhoneNumberTest.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace VasilDakov\SpeedyTest\Model;
+
+/**
+ * Class ShipmentPhoneNumberTest
+ *
+ * @author Vasil Dakov <vasildakov@gmail.com>
+ * @copyright 2009-2022 Neutrino.bg
+ * @version 1.0
+ */
+class ShipmentPhoneNumberTest
+{
+
+}
