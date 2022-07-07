2e8db5e0b77c2ce22840050d2479c025d6ef60c9
Valentin Valkanov <valentinvalkanof@gmail.com>
2022-07-07 21:23:20 +0300

    Updated setters and getters

--- src/Model/ShipmentPhoneNumber.php
+++ src/Model/ShipmentPhoneNumber.php
@@ -14,20 +14,28 @@ namespace VasilDakov\Speedy\Model;
 class ShipmentPhoneNumber {
 
     private string $number;
+    private string $extension;
 
     public function __construct(string $number) 
     {
         $this->number = $number;
     }
     
-    public function getNumber(): string 
+    public function setNumber(string $number): self
+    {
+        $this->number = $number;
+        return $this;
+    }
+
+        public function getNumber(): string 
     {
         return $this->number;
     }
 
-    public function setExtension(string $extension): void 
+    public function setExtension(string $extension): self
     {
         $this->extension = $extension;
+        return $this;
     }
     
     public function getExtension(): string 
