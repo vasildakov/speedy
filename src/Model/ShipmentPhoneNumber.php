ccfdd2f782b86e7656541a1c6f454594b977fe66
Valentin Valkanov <valentinvalkanof@gmail.com>
2022-07-07 21:05:57 +0300

    Updated classes and unit tests

--- src/Model/ShipmentPhoneNumber.php
+++ src/Model/ShipmentPhoneNumber.php
@@ -11,7 +11,28 @@ namespace VasilDakov\Speedy\Model;
  * @copyright 2009-2022 Neutrino.bg
  * @version 1.0
  */
-class ShipmentPhoneNumber
-{
+class ShipmentPhoneNumber {
+
+    private string $number;
+
+    public function __construct(string $number) 
+    {
+        $this->number = $number;
+    }
+    
+    public function getNumber(): string 
+    {
+        return $this->number;
+    }
+
+    public function setExtension(string $extension): void 
+    {
+        $this->extension = $extension;
+    }
+    
+    public function getExtension(): string 
+    {
+        return $this->extension;
+    }
 
 }
--- test/Model/ShipmentPhoneNumberTest.php
+++ test/Model/ShipmentPhoneNumberTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace VasilDakov\SpeedyTest\Model;
 
 use PHPUnit\Framework\TestCase;
+use VasilDakov\Speedy\Model\ShipmentPhoneNumber;
 
 /**
  * Class ShipmentPhoneNumberTest
@@ -13,7 +14,18 @@ use PHPUnit\Framework\TestCase;
  * @copyright 2009-2022 Neutrino.bg
  * @version 1.0
  */
-class ShipmentPhoneNumberTest extends TestCase
+class ShipmentPhoneNumberTest extends TestCase 
 {
-
+    protected $number;
+    
+    protected function setUp(): void 
+    {
+        $this->number = '0888302050';
+    }
+    
+    public function testItCanBeCreated() 
+    {
+        $shipmentPhoneNumber = new ShipmentPhoneNumber($this->number);
+        $this->assertSame($this->number, $shipmentPhoneNumber->getNumber());
+    }
 }
