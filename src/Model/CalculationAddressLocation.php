c7c29df1fda684411b31d04a0c93f714127b7fbb
DESKTOP-D5DONSH\User <valentinvalkanof@gmail.com>
2022-07-15 14:37:45 +0300

    Test

--- src/Model/CalculationAddressLocation.php
+++ src/Model/CalculationAddressLocation.php
@@ -30,13 +30,80 @@ class CalculationAddressLocation
      *
      * @var CountryCode
      */
-    private CountryCode $countryId;
+    private ?CountryCode $countryId;
+    private string $stateId;
+    private int $siteId;
+    private ?string $siteType;
+    private ?string $siteName;
+    private string $postCode;
+    
+    public function __construct(string $stateId, int $siteId, string $postCode) 
+    {
+        $this->stateId = $stateId;
+        $this->siteId = $siteId;
+        $this->postCode = $postCode;
+    }
 
-
+    
     public function setCountryId(CountryCode $countryId): self
     {
         $this->countryId = $countryId;
 
         return $this;
+        
     }
+    public function getStateId(): string 
+    {
+        return $this->stateId;
+    }
+
+    public function getSiteId(): int 
+    {
+        return $this->siteId;
+    }
+
+    public function getSiteType(): string 
+    {
+        return $this->siteType;
+    }
+
+    public function getSiteName(): string 
+    {
+        return $this->siteName;
+    }
+
+    public function getPostCode(): string 
+    {
+        return $this->postCode;
+    }
+
+    public function setStateId(string $stateId): self 
+    {
+        $this->stateId = $stateId;
+        return $this;
+    }
+
+    public function setSiteId(int $siteId): self
+    {
+        $this->siteId = $siteId;
+        return $this;
+    }
+
+    public function setSiteType(string $siteType): self
+    {
+        $this->siteType = $siteType;
+        return $this;
+    }
+
+    public function setSiteName(string $siteName): self
+    {
+        $this->siteName = $siteName;
+        return $this;
+    }
+
+    public function setPostCode(string $postCode): self 
+    {
+        $this->postCode = $postCode;
+        return $this;
+    }  
 }
