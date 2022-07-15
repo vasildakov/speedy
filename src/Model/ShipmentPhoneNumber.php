06d1a95a98c542ee53e55ac313b96ef1f5fe3fb6
DESKTOP-D5DONSH\User <valentinvalkanof@gmail.com>
2022-07-15 17:31:21 +0300

    Updated class ShipmentPhoneNumber

--- src/Model/ShipmentPhoneNumber.php
+++ src/Model/ShipmentPhoneNumber.php
@@ -10,36 +10,62 @@ namespace VasilDakov\Speedy\Model;
  * @author Vasil Dakov <vasildakov@gmail.com>
  * @copyright 2009-2022 Neutrino.bg
  * @version 1.0
+ * @todo https://api.speedy.bg/web-api.html?fbclid=IwAR1pcF0uXsAZheAKesQcArBJ7QenkDVyEz36aXwfRGCQZvcInjIYMuE82E4#href-ds-shipment-phone-number
  */
 class ShipmentPhoneNumber {
-
+    
+    /**
+     * @var string
+     */
     private string $number;
     
+    /**
+     * @var string
+     */
     private ?string $extension = null;
-
+    
+    /**
+     * @param string $number
+     */
     public function __construct(string $number) 
     {
         $this->setNumber($number);
     }
     
+    /**
+     * @param string $number
+     * @return self
+     */
     private function setNumber(string $number): self
     {
         // validation goes here
         $this->number = $number;
+        
         return $this;
     }
-
+    
+    /**
+     * @return string number
+     */
     public function getNumber(): string 
     {
         return $this->number;
     }
-
+    
+    /**
+     * @param string $extension
+     * @return self
+     */
     public function setExtension(string $extension): self
     {
         $this->extension = $extension;
+        
         return $this;
     }
     
+    /**
+     * @return string extension
+     */
     public function getExtension(): ?string 
     {
         return $this->extension;
--- test/Model/ShipmentPhoneNumberTest.php
+++ test/Model/ShipmentPhoneNumberTest.php
@@ -13,11 +13,18 @@ use VasilDakov\Speedy\Model\ShipmentPhoneNumber;
  * @author Vasil Dakov <vasildakov@gmail.com>
  * @copyright 2009-2022 Neutrino.bg
  * @version 1.0
+ * @todo https://api.speedy.bg/web-api.html?fbclid=IwAR1pcF0uXsAZheAKesQcArBJ7QenkDVyEz36aXwfRGCQZvcInjIYMuE82E4#href-ds-shipment-phone-number
  */
 class ShipmentPhoneNumberTest extends TestCase 
 {
+    /**
+     * @var string
+     */
     protected $number;
     
+    /**
+     * @param string $number
+     */
     protected function setUp(): void 
     {
         $this->number = '0888302050';
