70eed5f5b5528af79ecb894061dd3807cb26dfd2
Vasil Dakov <vasildakov@gmail.com>
2022-07-16 23:12:59 +0300

    Updated unit tests with some comments left

--- test/Model/CalculationAddressLocationTest.php
+++ test/Model/CalculationAddressLocationTest.php
@@ -4,7 +4,16 @@ declare(strict_types=1);
 
 
 namespace VasilDakov\SpeedyTest\Model;
+
 use VasilDakov\Speedy\ValueObject\CountryCode;
+
+/*
+ * Note: Be careful with the namespaces, the unit test classes are in "VasilDakov\SpeedyTest"
+ * but the main classes are in "VasilDakov\Speedy"
+ */
+
+use VasilDakov\Speedy\Model\CalculationAddressLocation; // Note: The class was not imported.
+
 
 use PHPUnit\Framework\TestCase;
 
@@ -17,30 +26,77 @@ use PHPUnit\Framework\TestCase;
  */
 class CalculationAddressLocationTest extends TestCase
 {
+    protected $siteId;
+
+    protected $postCode;
+
+    protected $countryId;
+
     protected function setUp():void
     {
+        $this->siteId   = 100; // Note: some random integer value for now
+
+        $this->postCode = '1000'; // Note: some random string value for now
+
+        // Note: the countryId is an instance of CountryCode, so it has to be mocked
+        $this->countryId = $this->createMock(CountryCode::class);
+
         parent::setUp();
-        
     }
     
     /**
      * @group CalculationAddressLocation
-     * return void
      */
     public function testItCanBeCreated(): void
     {
-        $object = new CalculationAddressLocation($this->stateId, $this->siteId, $this->postCode);
-        $this->assertOfInstance(CalculationAddressLocation::class, $object);
-        
+        // Note: The testing variables $stateId, $siteId, etc., were not initialized in the constructor
+        $object = new CalculationAddressLocation($this->siteId, $this->postCode);
+
+        // Note: $this->assertOfInstance() must be $this->assertInstanceOf()
+        // Wrong: $this->assertOfInstance(CalculationAddressLocation::class, $object);
+
+        $this->assertInstanceOf(CalculationAddressLocation::class, $object);
+
     }
     
     /**
-     * 
-     * @return void
+     * @group CalculationAddressLocation
      */
-    public function testItCanRetrieveStateId() :void
+    public function testItCanRetrieveCountryId() :void
     {
-        $object = new CalculationAddressLocation($this->stateId, $this->siteId, $this->postCode); 
-        $this->assertEquals($this->stateId, $object->getStateId());
+        // Note: The testing variables $stateId, $siteId, etc., were not initialized in the constructor
+        $object = new CalculationAddressLocation($this->siteId, $this->postCode);
+
+        // Note: once you have the object created above, just use the setter
+        $object->setCountryId($this->countryId);
+
+        // Note: the variable returned by the getter must be the same as the one used with the setter
+        $this->assertEquals($this->countryId, $object->getCountryId());
+    }
+
+
+    /**
+     * Note: we are testing the stateId accessors here
+     * @group CalculationAddressLocation
+     */
+    public function testItCanRetrieveTheStateId()
+    {
+        // Note: since the stateId is passed as constructor argument, we don't use the setter here
+        $object = new CalculationAddressLocation($this->siteId, $this->postCode);
+
+        $this->assertEquals($this->siteId, $object->getSiteId());
+    }
+
+
+    /**
+     * Note: we are testing the postCode accessors here
+     * @group CalculationAddressLocation
+     */
+    public function testItCanRetrieveThePostCode()
+    {
+        // Note: since the postCode is passed as constructor argument, we don't use the setter here
+        $object = new CalculationAddressLocation($this->siteId, $this->postCode);
+
+        $this->assertEquals($this->postCode, $object->getPostCode());
     }
 }
