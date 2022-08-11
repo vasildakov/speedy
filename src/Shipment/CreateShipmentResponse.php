3f8c779dc6e0fe85aa1ce938f46ca138843dc49c
Vasil Dakov <vasildakov@gmail.com>
2022-08-11 15:06:26 +0300

    Updated shipment model

--- src/Shipment/CreateShipmentResponse.php
+++ src/Shipment/CreateShipmentResponse.php
@@ -16,16 +16,121 @@ use DateTime;
  */
 class CreateShipmentResponse
 {
+    /**
+     * @var string
+     */
     private string $id;
 
+    /**
+     * @var array
+     */
     private array $parcels;
 
+    /**
+     * @var ShipmentPrice
+     */
     private ShipmentPrice $price;
 
+    /**
+     * @var DateTime
+     */
     private DateTime $pickupDate;
 
+    /**
+     * @var DateTime
+     */
     private DateTime $deliveryDeadline;
 
+    /**
+     * @var
+     */
     private $error;
 
+
+    public function setId($id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getParcels(): array
+    {
+        return $this->parcels;
+    }
+
+    /**
+     * @param array $parcels
+     */
+    public function setParcels(array $parcels): void
+    {
+        $this->parcels = $parcels;
+    }
+
+    /**
+     * @return ShipmentPrice
+     */
+    public function getPrice(): ShipmentPrice
+    {
+        return $this->price;
+    }
+
+    /**
+     * @param ShipmentPrice $price
+     */
+    public function setPrice(ShipmentPrice $price): void
+    {
+        $this->price = $price;
+    }
+
+    /**
+     * @return DateTime
+     */
+    public function getPickupDate(): DateTime
+    {
+        return $this->pickupDate;
+    }
+
+    /**
+     * @param DateTime $pickupDate
+     */
+    public function setPickupDate(DateTime $pickupDate): void
+    {
+        $this->pickupDate = $pickupDate;
+    }
+
+    /**
+     * @return DateTime
+     */
+    public function getDeliveryDeadline(): DateTime
+    {
+        return $this->deliveryDeadline;
+    }
+
+    /**
+     * @param DateTime $deliveryDeadline
+     */
+    public function setDeliveryDeadline(DateTime $deliveryDeadline): void
+    {
+        $this->deliveryDeadline = $deliveryDeadline;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getError()
+    {
+        return $this->error;
+    }
+
+    /**
+     * @param mixed $error
+     */
+    public function setError($error): void
+    {
+        $this->error = $error;
+    }
 }
--- src/Shipment/MoneyTransferPremium.php
+++ src/Shipment/MoneyTransferPremium.php
@@ -1,7 +1,4 @@
-<?php
-
-declare(strict_types=1);
-
+<?php declare(strict_types=1);
 
 namespace VasilDakov\Speedy\Shipment;
 
@@ -14,5 +11,75 @@ namespace VasilDakov\Speedy\Shipment;
  */
 class MoneyTransferPremium
 {
+    /**
+     * @var float
+     */
+    private float $amount;
 
+    /**
+     * @var float
+     */
+    private float $amountLocal;
+
+    /**
+     * @var string
+     */
+    private string $payer;
+
+    /**
+     * @param float $amount
+     * @return $this
+     */
+    public function setAmount(float $amount): self
+    {
+        $this->amount = $amount;
+
+        return $this;
+    }
+
+    /**
+     * @return float
+     */
+    public function getAmount(): float
+    {
+        return $this->amount;
+    }
+
+    /**
+     * @param float $amountLocal
+     * @return $this
+     */
+    public function setAmountLocal(float $amountLocal): self
+    {
+        $this->amountLocal = $amountLocal;
+
+        return $this;
+    }
+
+    /**
+     * @return float
+     */
+    public function getAmountLocal(): float
+    {
+        return $this->amountLocal;
+    }
+
+    /**
+     * @param string $payer
+     * @return $this
+     */
+    public function setPayer(string $payer): self
+    {
+        $this->payer = $payer;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPayer(): string
+    {
+        return $this->payer;
+    }
 }
