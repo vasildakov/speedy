9d3d4b049831af791dae662f0136a28d85664183
Vasil Dakov <vasildakov@gmail.com>
2022-08-11 14:42:45 +0300

    Updated location country service

---
+++ test/Location/Country/FindCountryRequestTest.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace VasilDakov\SpeedyTest\Location\Country;
+
+/**
+ * Class FindCountryRequestTest
+ *
+ * @author Vasil Dakov <vasildakov@gmail.com>
+ * @copyright 2009-2022 Neutrino.bg
+ * @version 1.0
+ */
+class FindCountryRequestTest
+{
+
+}
