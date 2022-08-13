0475375a66c415f717a3d42afb13dd9146d5b3fc
Vasil Dakov <vasildakov@gmail.com>
2022-08-13 10:14:22 +0300

    Docs update

--- docs/conf.py
+++ docs/conf.py
@@ -8,14 +8,14 @@ lexers['php-annotations'] = PhpLexer(startinline=True, linenos=1)
 primary_domain = 'php'
 
 extensions = []
-templates_path = ['_templates']
+#templates_path = ['_templates']
 source_suffix = '.rst'
 master_doc = 'index'
 project = u'Speedy'
 copyright = u'2022, Vasil Dakov'
-version = '7'
+version = '1'
 html_title = "Speedy API Client Documentation"
 html_short_title = "Speedy API"
 
-exclude_patterns = ['_build']
-html_static_path = ['_static']
+#exclude_patterns = ['_build']
+#html_static_path = ['_static']
