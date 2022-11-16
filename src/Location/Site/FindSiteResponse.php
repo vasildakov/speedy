f658d66cb260af21151066e92cd4d5d9565ce851
Valentin Valkanov <valentinvalkanof@gmail.com>
2022-11-16 07:16:26 +0200

    Updating of the clases

--- src/Location/Site/FindSiteResponse.php
+++ src/Location/Site/FindSiteResponse.php
@@ -2,17 +2,68 @@
 
 declare(strict_types=1);
 
-
 namespace VasilDakov\Speedy\Location\Site;
+use VasilDakov\Speedy\Error;
+use VasilDakov\Speedy\Model\Site;
 
 /**
  * Class FindSiteResponse
- *
  * @author Vasil Dakov <vasildakov@gmail.com>
+ * @author Valentin Valkanov <valentinvalkanof@gmail.com>
  * @copyright 2009-2022 Neutrino.bg
  * @version 1.0
  */
 class FindSiteResponse
 {
 
+    private ?Site $site = null;
+
+    private ?Error $error =null;
+
+    public function __construct(?Site $site, ?Error $error)
+    {
+        $this->setSite($site);
+        $this->setError($error);
+    }
+
+    /**
+     * @return Site|null
+     */
+    public function getSite(): ?Site
+    {
+        return $this->site;
+    }
+
+    /**
+     * @param Site|null $site
+     */
+    public function setSite(?Site $site): void
+    {
+        $this->site = $site;
+    }
+
+    /**
+     * @return Error|null
+     */
+    public function getError(): ?Error
+    {
+        return $this->error;
+    }
+
+    /**
+     * @param Error|null $error
+     */
+    public function setError(?Error $error): void
+    {
+        $this->error = $error;
+    }
+
+    public function toArray(): array
+    {
+        return [
+
+        ];
+    }
+
 }
+
