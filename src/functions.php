a295643df82639d8c6cc64d11428734303f28ae4
Vasil Dakov <vasildakov@gmail.com>
2023-12-31 18:00:28 +0200

    phpcs

--- src/functions.php
+++ src/functions.php
@@ -3,14 +3,12 @@
 namespace VasilDakov\Speedy;
 
 // @codeCoverageIgnoreStart
-if (! \function_exists('json_validate')) {
+if (!\function_exists('json_validate')) {
     function json_validate(string $json, int $depth = 512, int $flags = 0): bool
     {
         if (0 !== $flags && \JSON_INVALID_UTF8_IGNORE !== $flags) {
-            throw new \ValueError(
-                'json_validate(): Argument #3 ($flags) must be a 
-                valid flag (allowed flags: JSON_INVALID_UTF8_IGNORE)'
-            );
+            throw new \ValueError('json_validate(): Argument #3 ($flags) must be a
+                valid flag (allowed flags: JSON_INVALID_UTF8_IGNORE)');
         }
 
         if ($depth <= 0) {
